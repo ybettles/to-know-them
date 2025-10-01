4ea48839b9a408e0a883977af99c8f3a82f7ca32
Yasmin <ypbettles@gmail.com>
2025-10-01 20:26:07 +0100

    Basic setup of window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use bevy::{prelude::*, window::WindowResolution};
+use std::{sync::LazyLock};
+
+static GAME_BACKGROUND_COLOR: LazyLock<Color> = LazyLock::new(|| Color::srgb_u8(56, 47, 30));
+
 fn main() {
-    println!("Hello, world!");
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins.set(ImagePlugin::default_nearest()).set(WindowPlugin {
+            primary_window: Some(Window {
+                title: format!("To Know Them {}", env!("CARGO_PKG_VERSION")),
+                resolution: WindowResolution::new(
+                    1600,
+                    900,
+                ),
+                ..default()
+            }),
+            ..default()
+        }),
+    // Add plugins here
+    );
+    app.add_systems(
+        Startup, setup_sys,
+        // Add other systems here
+    );
+
+    app.insert_resource(ClearColor(*GAME_BACKGROUND_COLOR));
+
+    app.run();
+}
+
+fn setup_sys(
+    mut cmd: Commands
+) {
+    cmd.spawn(Camera2d);
+    // Do other stuff here
 }
